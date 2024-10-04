4624f98345f0c5b921990e8b19fb5b815a81bacb
nanda ricco <nandaricco2677@gmail.com>
2024-10-04 14:48:19 +0700

    perubahan

--- Pemilihan2Percobaan114.java
+++ Pemilihan2Percobaan114.java
@@ -10,11 +10,25 @@ public class Pemilihan2Percobaan114 {
         System.out.print("Masukkan Tahun : ");
         tahun = empatbelas.nextInt();
 
+        // if ((tahun % 4) == 0) {
+        //     if ((tahun % 100) != 0) 
+        //         System.out.println("Tahun Kabisat");
+        //     else {
+        //         System.out.println("Bukan Tahun Kabisat");
+        //     }
+        // } else {
+        //     System.out.println("Bukan Tahun Kabisat");
+        // }
+
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) 
+            if ((tahun % 100) != 0) {
+                if (tahun % 400 == 0 ) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
                 System.out.println("Tahun Kabisat");
-            else {
-                System.out.println("Bukan Tahun Kabisat");
             }
         } else {
             System.out.println("Bukan Tahun Kabisat");

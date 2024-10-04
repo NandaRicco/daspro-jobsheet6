5e4f23e1dfda7bb302ab2c75a65e1ad4ab4d985a
nanda ricco <nandaricco2677@gmail.com>
2024-10-04 14:11:28 +0700

    tahun

---
+++ Pemilihan2Percobaan114.java
@@ -0,0 +1,23 @@
+package dasprojobsheet6;
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan114 {
+
+    public static void main(String[] args) {
+        Scanner empatbelas = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan Tahun : ");
+        tahun = empatbelas.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) 
+                System.out.println("Tahun Kabisat");
+            else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
